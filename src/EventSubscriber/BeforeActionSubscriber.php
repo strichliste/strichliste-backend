2eba24b9663112e42f3f7ed0fbf5e022cb12a1f7
schinken <schinken@bamberg.ccc.de>
2018-08-16 22:27:00 +0200

    Replace request with parsed json body automatically

---
+++ src/EventSubscriber/BeforeActionSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class BeforeActionSubscriber implements EventSubscriberInterface {
+
+    public static function getSubscribedEvents() {
+        return array(
+            KernelEvents::CONTROLLER => 'convertJsonStringToArray',
+        );
+    }
+
+    public function convertJsonStringToArray(FilterControllerEvent $event) {
+        $request = $event->getRequest();
+
+        if ($request->getContentType() != 'json' || !$request->getContent()) {
+            return;
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new BadRequestHttpException('invalid json body: ' . json_last_error_msg());
+        }
+
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+
+}
