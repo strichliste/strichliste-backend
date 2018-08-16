c2389664462027f80e5a4daf31ef37f84285d9f1
schinken <schinken@bamberg.ccc.de>
2018-08-16 23:26:01 +0200

    Implement TransactionController

---
+++ src/Controller/TransactionController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Transaction;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class TransactionController extends AbstractController
+{
+    /**
+     * @Route("/transaction", methods="GET")
+     */
+    public function list(Request $request, EntityManagerInterface $entityManager)
+    {
+        $offset = $request->request->get('offset', 0);
+        $limit = $request->request->get('limit', 25);
+
+        $transactions = $entityManager->getRepository(Transaction::class)->findAll($offset, $limit);
+
+        return $this->json([
+            'transactions' => $transactions
+        ]);
+    }
+
+    /**
+     * @Route("/user/{userId}/transaction", methods="POST")
+     */
+    public function createUserTransactions($userId, Request $request, EntityManagerInterface $entityManager)
+    {
+        /**
+         * @var User $user
+         */
+        $user = $entityManager->getRepository(User::class)->find($userId);
+
+        if (!$user) {
+            throw $this->createNotFoundException();
+        }
+
+        $transaction = new Transaction();
+        $transaction->setUser($user);
+
+        $amount = $request->request->get('amount');
+        // TODO: Validate transaction boundaries
+        $transaction->setAmount($amount);
+
+        // TODO: User something like bignumber
+        $user->setBalance($user->getBalance() + $amount);
+
+        $comment = $request->request->get('comment');
+        if ($comment) {
+            $transaction->setComment($comment);
+        }
+
+        $articleId = $request->request->get('articleId');
+        if ($articleId) {
+            $article = $entityManager->getRepository(Article::class)->find($articleId);
+
+            if (!$article) {
+                throw new BadRequestHttpException(sprintf('Article id %d not found', $articleId));
+            }
+
+            $transaction->setArticle($article);
+        }
+
+        $entityManager->transactional(function () use ($entityManager, $user, $transaction) {
+            $entityManager->persist($user);
+            $entityManager->persist($transaction);
+        });
+
+        $entityManager->flush();
+
+        return $this->json([
+            'transaction' => $transaction,
+        ]);
+    }
+
+    /**
+     * @Route("/user/{userId}/transaction", methods="GET")
+     */
+    public function getUserTransactions($userId, Request $request, EntityManagerInterface $entityManager)
+    {
+        $offset = $request->request->get('offset', 0);
+        $limit = $request->request->get('limit', 25);
+
+        $user = $entityManager->getRepository(User::class)->find($userId, $offset, $limit);
+
+        if (!$user) {
+            throw $this->createNotFoundException();
+        }
+
+        $transactions = $entityManager->getRepository(Transaction::class)->findByUser($user);
+
+        return $this->json([
+            'transactions' => $transactions,
+        ]);
+    }
+}
