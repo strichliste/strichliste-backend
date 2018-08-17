0aeb3198fb8160ee40b1598e981bd439a76273a0
schinken <schinken@bamberg.ccc.de>
2018-08-17 15:07:46 +0200

    Make username unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,7 +22,7 @@ class User implements \JsonSerializable {
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $name;
 
