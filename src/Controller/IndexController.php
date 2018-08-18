3fc90ab9f80af9ac9698d96dc802d06d2e0654a6
schinken <schinken@bamberg.ccc.de>
2018-08-18 20:18:20 +0200

    Serve index.html from /

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index() {
+        $webroot = $this->getParameter('webroot');
+        $index = sprintf("%s/index.html", $webroot);
+
+        if (!file_exists($index)) {
+            return new Response("Front-End is missing!");
+        }
+
+        return new Response(file_get_contents($index));
+    }
+}
