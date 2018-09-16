4c77d80976f9963b80f60efee594e9039ffa952d
schinken <schinken@bamberg.ccc.de>
2018-09-16 17:18:08 +0200

    Use isActive instead of isStale

--- src/Serializer/UserSerializer.php
+++ src/Serializer/UserSerializer.php
@@ -23,7 +23,7 @@ class UserSerializer {
             'active' => $user->isActive(),
             'email' => $user->getEmail(),
             'balance' => $user->getBalance(),
-            'isStale' => $this->userService->isStale($user),
+            'isActive' => $this->userService->isActive($user),
             'created' => $user->getCreated()->format('Y-m-d H:i:s'),
             'updated' => $user->getUpdated() ? $user->getUpdated()->format('Y-m-d H:i:s') : null
         ];
