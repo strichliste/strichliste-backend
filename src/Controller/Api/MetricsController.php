0cdf50973169a1cdfe13db4783294b3e1f24a64f
schinken <schinken@bamberg.ccc.de>
2018-12-22 20:01:20 +0100

    Fix #3: Implement metrics for user

--- src/Controller/Api/MetricsController.php
+++ src/Controller/Api/MetricsController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Article;
 use App\Entity\Transaction;
 use App\Entity\User;
+use App\Exception\UserNotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Query\Expr\Join;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/api/metrics")
- */
 class MetricsController extends AbstractController {
 
     /**
-     * @Route(methods="GET")
+     * @Route("/api/metrics", methods="GET")
      */
     function metrics(EntityManagerInterface $entityManager) {
         return $this->json([
@@ -22,6 +24,70 @@ class MetricsController extends AbstractController {
             'transactionCount' => $this->getTransactionCount($entityManager),
             'userCount' => $this->getUserCount($entityManager),
             'days' => $this->getTransactionsPerDay($entityManager)
+        ]);
+    }
+
+    /**
+     * @Route("/api/user/{userId}/metrics", methods="GET")
+     */
+
+    function userMetrics($userId, EntityManagerInterface $entityManager) {
+        /**
+         * @var $user User
+         */
+        $user = $entityManager->getRepository(User::class)->findByIdentifier($userId);
+
+        if (!$user) {
+            throw new UserNotFoundException($userId);
+        }
+
+        $articles = $entityManager->createQueryBuilder()
+            ->select('COUNT(a.id) as count, SUM(t.amount) * -1 as amount, a.name')
+            ->from(Transaction::class, 't')
+            ->innerJoin(Article::class, 'a', Join::WITH, 'a = t.article')
+            ->where('t.user = :user')
+            ->andWhere('t.article IS NOT NULL')
+            ->setParameter('user', $user)
+            ->groupBy('a.id')
+            ->orderBy('COUNT(a)', 'DESC')
+            ->getQuery()
+            ->getArrayResult();
+
+        $transactionCount = $entityManager->createQueryBuilder()
+            ->select('COUNT(t.id) as count')
+            ->from(Transaction::class, 't')
+            ->where('t.user = :user')
+            ->andWhere('t.deleted = false')
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        $outgoingTransactions = $this->getUserTransactionBaseQuery($user, $entityManager)
+            ->andWhere('t.recipientTransaction IS NOT NULL')
+            ->getQuery()
+            ->getOneOrNullResult(Query::HYDRATE_ARRAY);
+
+        $incomingTransactions = $this->getUserTransactionBaseQuery($user, $entityManager)
+            ->andWhere('t.senderTransaction IS NOT NULL')
+            ->getQuery()
+            ->getOneOrNullResult(Query::HYDRATE_ARRAY);
+
+        return $this->json([
+            'balance' => $user->getBalance(),
+
+            'articles' => array_map(function ($article) {
+                return [
+                    'name' => $article['name'],
+                    'count' => (int) $article['count'],
+                    'amount' => (int) $article['amount'],
+                 ];
+            }, $articles),
+
+            'transactions' => [
+                'count' => (int) $transactionCount,
+                'outgoing' => ['count' => (int) $outgoingTransactions['count'], 'amount' => (int) $outgoingTransactions['amount']],
+                'incoming' => ['count' => (int) $incomingTransactions['count'], 'amount' => (int) $incomingTransactions['amount']],
+            ]
         ]);
     }
 
@@ -88,5 +154,15 @@ class MetricsController extends AbstractController {
             ->from(User::class, 'u')
             ->getQuery()
             ->getSingleScalarResult();
+    }
+
+    private function getUserTransactionBaseQuery(User $user, EntityManagerInterface $entityManager) {
+        return $entityManager->createQueryBuilder()
+            ->select('COUNT(t.id) as count, SUM(t.amount) amount')
+            ->from(Transaction::class, 't')
+            ->where('t.user = :user')
+            ->andWhere('t.deleted = false')
+            ->groupBy('t.id')
+            ->setParameter('user', $user);
     }
 }
