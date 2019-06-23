122fec5456e96d2e0320efa824209dd13260e71a
schinken <schinken@bamberg.ccc.de>
2019-06-23 18:12:47 +0200

    Fix #52: Add no-cache header to API responses

---
+++ src/EventSubscriber/ApiResponseSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+
+class ApiResponseSubscriber implements EventSubscriberInterface {
+
+    function onKernelResponse(FilterResponseEvent $event) {
+        $response = $event->getResponse();
+
+        $response->headers->addCacheControlDirective('no-cache', true);
+        $response->headers->addCacheControlDirective('max-age', 0);
+        $response->headers->addCacheControlDirective('must-revalidate', true);
+        $response->headers->addCacheControlDirective('no-store', true);
+    }
+
+     static function getSubscribedEvents() {
+        return [
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        ];
+    }
+}
