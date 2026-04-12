5099221f16e7b515752e5cb37857a62e8b3f9490
Micha Ober <git@ober-mail.de>
2026-04-12 21:10:45 +0200

    Replace Route Annotations with Attributes

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,13 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController {
 
-    /**
-     * @Route("/", name="index")
-     */
+    #[Route('/', name: 'index')]
     public function index() {
         $webroot = $this->getParameter('webroot');
         $index = sprintf("%s/index.html", $webroot);
