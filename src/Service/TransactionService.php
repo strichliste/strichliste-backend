d5229bbafe680b75fdbf9ed619500acf231ed38e
Micha Ober <git@ober-mail.de>
2026-04-12 21:17:19 +0200

    Replace deprecated `transactional` with `wrapInTransaction`

--- src/Service/TransactionService.php
+++ src/Service/TransactionService.php
@@ -76,7 +76,7 @@ class TransactionService {
             throw new TransactionInvalidException('Amount can\'t be positive when sending money or buying an article');
         }
 
-        return $this->entityManager->transactional(function () use ($user, $amount, $comment, $quantity, $articleId, $recipientId) {
+        return $this->entityManager->wrapInTransaction(function () use ($user, $amount, $comment, $quantity, $articleId, $recipientId) {
             $transaction = new Transaction();
             $transaction->setUser($user);
             $transaction->setComment($comment);
@@ -149,7 +149,7 @@ class TransactionService {
      * @return Transaction
      */
     function revertTransaction(int $transactionId): Transaction {
-        return $this->entityManager->transactional(function () use ($transactionId) {
+        return $this->entityManager->wrapInTransaction(function () use ($transactionId) {
 
             $transaction = $this->entityManager->getRepository(Transaction::class)->find($transactionId, LockMode::PESSIMISTIC_WRITE);
             if (!$transaction) {
