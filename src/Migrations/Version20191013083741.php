d3b253ee4a73b228515814e65b92a459d16a274f
Micha Ober <git@ober-mail.de>
2026-04-20 23:40:51 +0200

    Mark columns as non-nullable

--- src/Migrations/Version20191013083741.php
+++ src/Migrations/Version20191013083741.php
@@ -40,12 +40,12 @@ final class Version20191013083741 extends AbstractMigration {
     }
 
     private function upMySQL(): void {
-        $this->addSql('CREATE TABLE barcode (id INT AUTO_INCREMENT NOT NULL, article_id INT DEFAULT NULL, barcode VARCHAR(32) NOT NULL, created DATETIME NOT NULL, INDEX IDX_97AE02667294869C (article_id), UNIQUE INDEX UNIQ_barcode (barcode), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE barcode (id INT AUTO_INCREMENT NOT NULL, article_id INT NOT NULL, barcode VARCHAR(32) NOT NULL, created DATETIME NOT NULL, INDEX IDX_97AE02667294869C (article_id), UNIQUE INDEX UNIQ_barcode (barcode), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE barcode ADD CONSTRAINT FK_97AE02667294869C FOREIGN KEY (article_id) REFERENCES article (id)');
 
         $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, article_id INT DEFAULT NULL, tag VARCHAR(255) NOT NULL, created DATETIME NOT NULL, INDEX IDX_389B7837294869C (article_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
 
-        $this->addSql('CREATE TABLE article_tag (id INT AUTO_INCREMENT NOT NULL, article_id INT DEFAULT NULL, tag_id INT DEFAULT NULL, created DATETIME NOT NULL, INDEX IDX_919694F97294869C (article_id), INDEX IDX_919694F9BAD26311 (tag_id), UNIQUE INDEX UNIQ_article_tag (article_id, tag_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article_tag (id INT AUTO_INCREMENT NOT NULL, article_id INT NOT NULL, tag_id INT NOT NULL, created DATETIME NOT NULL, INDEX IDX_919694F97294869C (article_id), INDEX IDX_919694F9BAD26311 (tag_id), UNIQUE INDEX UNIQ_article_tag (article_id, tag_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE article_tag ADD CONSTRAINT FK_919694F97294869C FOREIGN KEY (article_id) REFERENCES article (id)');
         $this->addSql('ALTER TABLE article_tag ADD CONSTRAINT FK_919694F9BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id)');
 
@@ -58,12 +58,12 @@ final class Version20191013083741 extends AbstractMigration {
     private function upSQLite() {
         $this->addSql('CREATE TABLE tag (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, tag VARCHAR(255) NOT NULL, created DATETIME NOT NULL)');
 
-        $this->addSql('CREATE TABLE article_tag (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, article_id INTEGER DEFAULT NULL, tag_id INTEGER DEFAULT NULL, created DATETIME NOT NULL, CONSTRAINT FK_919694F97294869C FOREIGN KEY (article_id) REFERENCES article (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_919694F9BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE TABLE article_tag (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, article_id INTEGER NOT NULL, tag_id INTEGER NOT NULL, created DATETIME NOT NULL, CONSTRAINT FK_919694F97294869C FOREIGN KEY (article_id) REFERENCES article (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_919694F9BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_919694F97294869C ON article_tag (article_id)');
         $this->addSql('CREATE INDEX IDX_919694F9BAD26311 ON article_tag (tag_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_article_tag ON article_tag (article_id, tag_id)');
 
-        $this->addSql('CREATE TABLE barcode (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, article_id INTEGER DEFAULT NULL, barcode VARCHAR(32) NOT NULL, created DATETIME NOT NULL, CONSTRAINT FK_97AE02667294869C FOREIGN KEY (article_id) REFERENCES article (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE TABLE barcode (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, article_id INTEGER NOT NULL, barcode VARCHAR(32) NOT NULL, created DATETIME NOT NULL, CONSTRAINT FK_97AE02667294869C FOREIGN KEY (article_id) REFERENCES article (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE INDEX IDX_97AE02667294869C ON barcode (article_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_barcode ON barcode (barcode)');
 
@@ -84,13 +84,13 @@ final class Version20191013083741 extends AbstractMigration {
         $this->addSql('CREATE SEQUENCE barcode_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE tag_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
 
-        $this->addSql('CREATE TABLE barcode (id INT NOT NULL, article_id INT DEFAULT NULL, barcode VARCHAR(32) NOT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE barcode (id INT NOT NULL, article_id INT NOT NULL, barcode VARCHAR(32) NOT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_97AE02667294869C ON barcode (article_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_barcode ON barcode (barcode)');
 
         $this->addSql('CREATE TABLE tag (id INT NOT NULL, tag VARCHAR(255) NOT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
 
-        $this->addSql('CREATE TABLE article_tag (id INT NOT NULL, article_id INT DEFAULT NULL, tag_id INT DEFAULT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE article_tag (id INT NOT NULL, article_id INT NOT NULL, tag_id INT NOT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_919694F97294869C ON article_tag (article_id)');
         $this->addSql('CREATE INDEX IDX_919694F9BAD26311 ON article_tag (tag_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_article_tag ON article_tag (article_id, tag_id)');
